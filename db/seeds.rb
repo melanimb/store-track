f3b25aa5f8a185f16c293b4e40cf1ac15287827e
melanimb <92109474+melanimb@users.noreply.github.com>
2023-05-19 12:28:25 +0200

    feat: products index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 20.times do
   Client.create(name: Faker::Name.name, email: Faker::Internet.email, address: Faker::Address.full_address, phone: Faker::PhoneNumber.cell_phone, store_id: 1)
 end
+
+20.times do
+  Product.create(name: Faker::Commerce.product_name, description: Faker::Commerce.department(max: 5), price: Faker::Commerce.price(range: 0..10.0, as_string: true), quantity: Faker::Number.between(from: 1, to: 100), store_id: 1)
+end
