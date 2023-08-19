3a42756a32c4e347edbf11c015c928d2d5e00504
melanimb <92109474+melanimb@users.noreply.github.com>
2023-08-19 13:05:26 +0200

    Revert "render"
    This reverts commit cb3fffe54a5e64842380882692bfc2e76eaf7977.

--- config/puma.rb
+++ config/puma.rb
@@ -24,8 +24,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-workers ENV.fetch("WEB_CONCURRENCY") { 4 }
-
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
@@ -40,6 +38,6 @@ workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 # process behavior so workers use less memory.
 #
 # preload_app!
-preload_app!
+
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
