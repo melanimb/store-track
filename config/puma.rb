cb3fffe54a5e64842380882692bfc2e76eaf7977
melanimb <92109474+melanimb@users.noreply.github.com>
2023-08-19 13:02:00 +0200

    render

--- config/puma.rb
+++ config/puma.rb
@@ -24,6 +24,8 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
@@ -38,6 +40,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # process behavior so workers use less memory.
 #
 # preload_app!
-
+preload_app!
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
